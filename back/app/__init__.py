9b0b6ca0aa9d8f7256f9139922671f7f23a3b7b3
sichoi <clogic@toss.im>
2020-01-26 12:32:28 +0900

    back, enhance(-): requirements 수정, redis 연결

--- back/app/__init__.py
+++ back/app/__init__.py
@@ -1,14 +1,17 @@
 # flake8: noqa
 # isort:skip_file
 from flask import Flask
+from flask_session import Session
 
 from app import db
 from app.config import Config
 from flask_cors import CORS
 
+
 app = Flask(__name__)
-app.secret_key = Config.SECRET_KEY
+app.config.from_object(Config)
 CORS(app, supports_credentials=True)
+session = Session(app)
 
 
 from app.api import *
